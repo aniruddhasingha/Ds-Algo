d24ea2ddf1c891a4707e21c6ef73401f3d92f773
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-03-06 07:49:35 +0000

    pushZerosToEnd to end

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const nums = [ -2, -3, 4, -1, -2, 1, 5, -3 ];
-       function maxSubArraySum(a, size)
-{
-    var maxint = Math.min()
-    var max_so_far = -maxint - 1
-    var max_ending_here = 0
-      
-    for (var i = 0; i < size; i++)
-    {
-        max_ending_here = max_ending_here + nums[i]
-      console.log( max_ending_here )
-      console.log(max_so_far)
-      console.log(max_so_far < max_ending_here ? true :false)
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here
- 
-        if (max_ending_here < 0)
-            max_ending_here = 0
+function pushZerosToEnd(arr, n) 
+{ 
+    let count = 0; 
+    for (let i = 0; i < n; i++) {
+        if (arr[i] != 0){ 
+        console.log("+++===+++",count)
+            arr[count++] = arr[i]; 
+        console.log("---===---",count)
+        }
     }
-  console.log(max_so_far)
-    return max_so_far
-}
-
-maxSubArraySum(nums,nums.length);
+    while (count < n) {
+        arr[count++] = 0; 
+    }
+  console.log(arr);
+} 
+  
+    let arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]; 
+    let n = arr.length; 
+    pushZerosToEnd(arr, n); 
