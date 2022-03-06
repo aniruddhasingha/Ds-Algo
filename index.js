ae1ac3034e8067ab1c17c9f8ec7bb6dc2c9138db
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-03-06 07:41:59 +0000

    MaxSubArray

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-// reverse string
-function reverseStr(text){
-  if(!text || text.length < 2 || typeof text !== 'string'){
-  console.log("Not a string");
-    return 'Not a string' 
-  }
-  let reversedString = "";
-  for(let i=text.length; i>0; i--){
-    let char = text[i-1];
-    reversedString+= char;
-  }
-  console.log(reversedString);
-  return reversedString;
+const nums = [ -2, -3, 4, -1, -2, 1, 5, -3 ];
+       function maxSubArraySum(a, size)
+{
+    var maxint = Math.min()
+    var max_so_far = -maxint - 1
+    var max_ending_here = 0
+      
+    for (var i = 0; i < size; i++)
+    {
+        max_ending_here = max_ending_here + nums[i]
+      console.log( max_ending_here )
+      console.log(max_so_far)
+      console.log(max_so_far < max_ending_here ? true :false)
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0
+    }
+  console.log(max_so_far)
+    return max_so_far
 }
 
-function reverse2(text){
-  if(!text || text.length < 2 || typeof text !== 'string'){
-  console.log("Not a string");
-    return 'Not a string' 
-  }
-  console.log(text.split('').reverse().join(''))
-  return text.split('').reverse().join('')
-}
-
-reverse2("my name is aniruddha");
+maxSubArraySum(nums,nums.length);
